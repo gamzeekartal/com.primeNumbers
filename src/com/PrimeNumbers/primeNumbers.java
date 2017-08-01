6e756ef4feb17adec9118d13c02e3c3d2ccdb81b
gamze kartal <gamzeekartal@gmail.com>
2017-08-01 15:08:22 +0300

    task8

---
+++ src/com/PrimeNumbers/primeNumbers.java
@@ -0,0 +1,34 @@
+package com.PrimeNumbers;
+
+import java.util.Scanner;
+
+public class primeNumbers {
+
+	private static Scanner in;
+
+	public static void main(String[] args) {
+		
+		System.out.print("please enter a number: ");
+		in = new Scanner(System.in);
+		int number = in.nextInt();
+			
+		for(int i=1; i<=number; i++){
+			if(primeNumbers.primeNumbers(i) == true)
+				System.out.println(i + " is prime");
+		}
+	}
+	
+	public static boolean primeNumbers(int num){
+		
+		if (num<2){
+			return false;
+		}
+		for(int i=3; i<num; i++){
+			if(num%i==0)
+				return false;	
+			}
+		return true;
+		}
+		
+	}
+
